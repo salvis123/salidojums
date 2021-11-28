e6cbbfbf1080ad621f20430df4c615f416f44911
Glitch (salidojuma-anketa) <none>
2021-11-28 12:00:46 +0000

    👟🎆 Checkpoint ./connect.php:15570000/1751 ./index.html:15570000/650 ./pieteikties.html:15570000/663

--- connect.php
+++ connect.php
@@ -1,12 +1,15 @@
 <?php
-if(isset($_POST['submit'])){
-$firstName = "firstname:".$_POST['firstName']."
-";
-$lastName = "lastname:".$_POST['lastName']."
-";
-$file=fopen("file.txt", "a");
-fwrite($file, $firstName);
-fwrite($file, $lastName);
-fclose($file);
+
+if(isset($_POST["submit"]))  //check submit button is pressed
+{
+
+$name = $_POST["name"];  // get name from URL
+
+echo($name);  //Print Name
+
+$file = Fopen("db.txt","a+"); //create a file if not exists
+
+fwrite($file,"\n".$name); // fwrite(file_object, data to store);
+
 }
 ?>
